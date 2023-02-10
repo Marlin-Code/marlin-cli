f0f6187e92bc222dc6dbb692df29c3fc89c28715
George Cooper <george@marlincode.com>
2023-02-10 10:35:40 -0500

    use api

--- marlin-cli/api/archetypes.py
+++ marlin-cli/api/archetypes.py
@@ -1,25 +1,8 @@
-ARCHETYPE_MAP = {
-  "react-js": {
-    "creator": "Marlin",
-    "description": "desc",
-    "documentation_url": "https://marlincode.notion.site/Jumpstart-Your-Frontend-3788900c2f2843a29da725fbbb3d6aa1",
-    "repository": {
-      "owner": "Marlin-Code",
-      "repo_name": "react_frontend_module",
-      "version": "1.0.0"
-    }
-  },
-  "serverless-api": {
-    "creator": "Marlin",
-    "description": "desc",
-    "documentation_url": "https://marlincode.notion.site/Jumpstart-Your-API-3f51845ff96d46c78ceefef89a5b71c8",
-    "repository": {
-      "owner": "Marlin-Code",
-      "repo_name": "serverless-api",
-      "version": "1.0.0"
-    }
-  }
-}
+import requests
+import constants 
 
 def get_archetype(archetype_name):
-  return ARCHETYPE_MAP.get(archetype_name)
+  response = requests.get(
+    url=f"{constants.API_URL}/archetypes/{archetype_name}"
+  )
+  return response.json()
--- marlin-cli/commands/docs.py
+++ marlin-cli/commands/docs.py
@@ -1,12 +1,16 @@
 import click
 import sys
 from api import archetypes
+import constants
+import os
 
 @click.command()
 @click.argument('NAME')
 def docs(name):
   """Fetches the documentation url for the Marlin archetype or module NAME"""
   archetype_details = archetypes.get_archetype(archetype_name=name)
+  click.echo(constants.API_URL)
+
   if (not(archetype_details)):
     click.echo(click.style("The requested entity does not exist.", fg="red"))
     sys.exit(1)
