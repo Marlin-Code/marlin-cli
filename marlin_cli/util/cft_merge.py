1477fdea09f75019a915612372ec8e8d57b75d82
TimLaRowe <tlarowe10@gmail.com>
2023-02-27 18:33:00 -0500

    fmt

--- marlin_cli/util/cft_merge.py
+++ marlin_cli/util/cft_merge.py
@@ -1,27 +1,33 @@
 import click
 from cfn_tools import load_yaml, dump_yaml
 
+
 def cft_merge(source, destination):
     """
     Add source objects from a CFT to destination CFT
     """
     with open(source, "r") as s:
-      raw = s.read()
-      source_cft = load_yaml(raw)
+        raw = s.read()
+        source_cft = load_yaml(raw)
     with open(destination, "r") as d:
-      raw =  d.read()
-      destination_cft = load_yaml(raw)
-    
+        raw = d.read()
+        destination_cft = load_yaml(raw)
+
     if not destination_cft:
-        click.echo(click.style(f'Unable to parse yaml file. Ensure that it is properly formatted and try again.', fg='red'))
-        raise Exception('ParseException: Unable to parse yaml file.')
-    
+        click.echo(
+            click.style(
+                f"Unable to parse yaml file. Ensure that it is properly formatted and try again.",
+                fg="red",
+            )
+        )
+        raise Exception("ParseException: Unable to parse yaml file.")
+
     def item_gen(source, dest):
         if isinstance(source, dict):
             for key, val in source.items():
                 if not dest or key not in dest:
-                  dest[key] = val
-                
+                    dest[key] = val
+
                 item_gen(val, dest[key])
 
     item_gen(source_cft, destination_cft)
--- marlin_cli/util/yaml_merge.py
+++ marlin_cli/util/yaml_merge.py
@@ -1,5 +1,6 @@
 import yaml
-import click 
+import click
+
 
 def yaml_merge(source, destination):
     """
@@ -9,17 +10,22 @@ def yaml_merge(source, destination):
         source_yaml = yaml.safe_load(s)
     with open(destination, "r") as d:
         destination_yaml = yaml.safe_load(d)
-    
+
     if not destination_yaml:
-        click.echo(click.style(f'Unable to parse yaml file. Ensure that it is properly formatted and try again.', fg='red'))
-        raise Exception('ParseException: Unable to parse yaml file.')
-    
+        click.echo(
+            click.style(
+                f"Unable to parse yaml file. Ensure that it is properly formatted and try again.",
+                fg="red",
+            )
+        )
+        raise Exception("ParseException: Unable to parse yaml file.")
+
     def item_gen(source, dest):
         if isinstance(source, dict):
             for key, val in source.items():
                 if not dest or key not in dest:
                     dest[key] = val
-                
+
                 item_gen(val, dest[key])
 
     item_gen(source_yaml, destination_yaml)
